ba45bc60312a6a12ff0c5d9206ace44a918dd8c1
Name <email>
2025-01-24 17:10:03 -0500

    First Commit

--- Assignment2.py
+++ Assignment2.py
@@ -1,4 +1,4 @@
-import keyword
+import keyword # Imports a tool that can be used to check if a string is a keyword in python
 
-print(keyword.iskeyword("type"))
-print(keyword.iskeyword("and"))
+print(keyword.iskeyword("Hello World")) # This string should return a False
+print(keyword.iskeyword("and")) # This string should return a True
